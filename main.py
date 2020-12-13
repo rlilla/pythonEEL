4405cfe41af3fc4dea206c2feebe1d0231904119
rlilla <ricardo_lilla@hotmail.com>
2020-12-13 15:53:52 -0300

    Final

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ altura = GetSystemMetrics(1)
 
 eel.init('web')
 df=pd.read_csv("C://temp//teste.txt",sep=";")
+df1=pd.read_excel("NCM.xls",header=[1])
 
 @eel.expose
 def teste(var1):
@@ -14,9 +15,12 @@ def teste(var1):
 @eel.expose
 def consultar(descricao):
     x = df[df['descricao'].str.contains(descricao) | df['codigo'].str.contains(descricao)].to_dict()
-    y=[10,20]
+    return x
+@eel.expose
+def consultarNCM(codigo):
+    x = df1[df1['NCM']==codigo].to_dict()
     print(x)
     return x
 
-
+consultarNCM(1012100)
 eel.start('main.html',size=(largura, altura))
